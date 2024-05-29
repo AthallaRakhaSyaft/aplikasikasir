427dc62ab4dd1214a6901cecc97da8f4b03036a4
athalla rakha syafa'at <81074924+AthallaRakhaSyaft@users.noreply.github.com>
2024-05-29 11:48:17 +0700

    Update createdata.php

--- createdata.php
+++ createdata.php
@@ -1,21 +1,34 @@
 <?php
 if (isset($_POST['submit'])) {
+    // Ambil nilai dari form
     $vid = $_POST['vid'];
     $vnama = $_POST['vnama'];
     $vkehadiran = $_POST['vkehadiran'];
-    
+    $vtanggal = date('Y-m-d'); // Mengambil tanggal saat ini dengan format Y-m-d
+
+    // Sisipkan koneksi ke database
     include "conn.php";
-    $query = "INSERT INTO admin VALUES ('$vid', '$vnama', '$vkehadiran')";
+
+    // Buat pernyataan SQL
+    $query = "INSERT INTO admin (id, nama, kehadiran, tanggal) VALUES ('$vid', '$vnama', '$vkehadiran', '$vtanggal')";
+    
+    // Jalankan pernyataan SQL
     $result = mysqli_query($conn, $query);
+
+    // Periksa apakah data berhasil disimpan
     if ($result) {
         echo "<script>
-                alert('Data Berhasil Diinput');window.location='retrieve.php';
-                </script>";           
+                alert('Data Berhasil Diinput');
+                window.location='retrieve.php';
+              </script>";           
     } else {
         echo "<script>
-                alert('Data Gagal Diinput');window.location='createdata.php';
-                </script>";
+                alert('Data Gagal Diinput');
+                window.location='createdata.php';
+              </script>";
     }
+
+    // Tutup koneksi ke database
     mysqli_close($conn);
 } else { 
 ?>               
@@ -23,7 +36,7 @@ if (isset($_POST['submit'])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Form Input Admin</title>
+    <title>Form Input Kasir</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
@@ -42,9 +55,28 @@ if (isset($_POST['submit'])) {
                 <label for="vkehadiran">Kehadiran:</label>
                 <input type="text" name="vkehadiran" id="vkehadiran" class="input-field">
             </div>
+
+            <div class="form-group">
+            <b><p>Tanggal:</p></b>
+            <div id="datetime"></div>
+
+            <p></p>
             <input type="submit" name="submit" value="Simpan" class="submit-button">
         </form>
     </div>
+    
+    <script>
+     function displayDateTime() {
+        var now = new Date();
+        var date = now.toLocaleDateString();
+        
+        document.getElementById('datetime').innerHTML = date; // Menampilkan tanggal dan waktu
+    }
+
+    setInterval(displayDateTime, 1000); // Memperbarui setiap detik
+   
+    </script>
+
 </body>
 </html>
 
